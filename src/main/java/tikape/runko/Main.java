2952e98ee0c839df5cfea102a1e4f9b4c84cda5b
Rovaniemi <mauri.karlin@helsinki.fi>
2017-03-02 20:44:24 +0200

    10 viestiä per sivu

--- src/main/java/tikape/runko/Main.java
+++ src/main/java/tikape/runko/Main.java
@@ -13,6 +13,7 @@ import tikape.runko.database.KeskusteluDao;
 import tikape.runko.database.ViestiDao;
 import tikape.runko.domain.Keskustelu;
 import tikape.runko.domain.Viesti;
+import tikape.runko.domain.Maara;
 
 public class Main {
 
@@ -28,43 +29,36 @@ public class Main {
         get("/", (req, res) -> {
             HashMap map = new HashMap<>();
             map.put("nakyma", kategoriaDao.luoAlkunakyma());
-
-
             return new ModelAndView(map, "index");
         }, new ThymeleafTemplateEngine());
-
 
         get("/kategoria/:name", (req, res) -> {
             HashMap map = new HashMap<>();
             List<Keskustelu> keskustelut = new ArrayList<>();
             int keskusteluId = keskusteluDao.findIdByName(req.params("name"));
             keskustelut = keskusteluDao.findAllByKategoria(keskusteluId);
-
             map.put("keskustelut", keskustelut);
             map.put("kategoria", kategoriaDao.findOne(keskusteluId));
-
             return new ModelAndView(map, "keskustelut");
         }, new ThymeleafTemplateEngine());
 
-
-
         get("kategoria/:kategoriaNimi/:keskusteluId", (req, res) -> {
             HashMap map = new HashMap<>();
-            List<Viesti> viestit = new ArrayList<>();
-            viestit = viestiDao.findAllByKeskustelu(Integer.parseInt(req.params("keskusteluId")));
-
-            map.put("viestit", viestit);
-            map.put("keskustelu", keskusteluDao.findOne(Integer.parseInt(req.params("keskusteluId"))));
+            String koko[] = req.params("keskusteluId").split("-");
+            List<Viesti> kaikkiViestit = viestiDao.findAllByKeskustelu(Integer.parseInt(koko[0]));
+            List<Viesti> kymmenenViestia = kymmenenViestia(kaikkiViestit,koko[1]);
+            map.put("viestit", kymmenenViestia);
+            map.put("keskustelu", keskusteluDao.findOne(Integer.parseInt(koko[0])));
             int kategoriaId = keskusteluDao.findIdByName(req.params("kategoriaNimi"));
             map.put("kategoria", kategoriaDao.findOne(kategoriaId));
-
+            map.put("maara", viestienMaara(kaikkiViestit,Integer.parseInt(koko[1])));
             return new ModelAndView(map, "viestit");
         }, new ThymeleafTemplateEngine());
 
         post("/kategoria/:kategoriaNimi/:keskusteluId", (req, res) -> {
-            Integer keskusteluId = Integer.parseInt(req.params(":keskusteluId"));
+            String[] koko = req.params("keskusteluId").split("-");
+            Integer keskusteluId = Integer.parseInt(koko[0]);
             String teksti = req.queryParams("teksti");
-
             String lahettaja = req.queryParams("lahettaja");
             viestiDao.addViesti(teksti + " t. " + lahettaja, keskusteluId);
             res.redirect("redirect:home");
@@ -87,4 +81,28 @@ public class Main {
 
     }
 
+    public static List<Viesti> kymmenenViestia(List<Viesti> kaikkiViestit, String numero){
+        int monesko = Integer.parseInt(numero);
+        List<Viesti> kymmenenViestia = new ArrayList<>();
+        for (int i = (monesko * 10) - 10; i < monesko * 10; i++) {
+            if(i >= kaikkiViestit.size()){
+                break;
+            } else {
+                kymmenenViestia.add(kaikkiViestit.get(i));
+            }
+        }
+        return kymmenenViestia;
+    }
+
+    public static List<Maara> viestienMaara(List<Viesti> kaikkiviestit, int tamanhetkinen){
+        List<Maara> lista = new ArrayList<Maara>();
+        for (int i = 0; i < kaikkiviestit.size(); i = i + 10) {
+            if((i/10 + 1) == tamanhetkinen){
+                lista.add(new Maara(i/10 + 1,true));
+            } else {
+                lista.add(new Maara(i/10 + 1,false));
+            }
+        }
+        return lista;
+    }
 }
