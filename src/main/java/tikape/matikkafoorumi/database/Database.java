ecdbf3da22fed70a2dcdab67878d8dd159525a1c
Maaret <maaret.ihamaki@hotmail.com>
2017-03-03 16:35:36 +0200

    foorumiin aluksi luotavien keskustelujen viestisisällöt muutettu keskusteluaiheeseen liittyviksi

--- src/main/java/tikape/matikkafoorumi/database/Database.java
+++ src/main/java/tikape/matikkafoorumi/database/Database.java
@@ -7,11 +7,11 @@ import java.util.List;
 /*
 Täällä luodaan yhteys meidän foorumin tietokantaan ja jos tietokantaa ei vielä 
 ole luotu, niin tämä koodi myös luo sen. 
-*/
-
+ */
 public class Database {
+
     private String databaseAddress;
-    
+
     public Database(String databaseAddress) throws ClassNotFoundException {
         this.databaseAddress = databaseAddress;
     }
@@ -42,7 +42,6 @@ public class Database {
     private List<String> sqliteLauseet() {
         ArrayList<String> lista = new ArrayList<>();
 
-        // tietokantataulujen sekä seed datan luomiseen tarvittavat komennot suoritusjärjestyksessä
         lista.add("CREATE TABLE Kategoria (id integer PRIMARY KEY, nimi varchar(200) NOT NULL, kuvaus varchar(500) NOT NULL);");
         lista.add("CREATE TABLE Keskustelu (id integer PRIMARY KEY, otsikko varchar(200), aika Timestamp DATETIME DEFAULT CURRENT_TIMESTAMP, kategoria integer, FOREIGN KEY (kategoria) REFERENCES Kategoria(id));");
         lista.add("CREATE TABLE Viesti(id integer PRIMARY KEY, viesti varchar(5000) NOT NULL, aika Timestamp DATETIME DEFAULT CURRENT_TIMESTAMP, keskustelu integer, FOREIGN KEY (keskustelu) REFERENCES Keskustelu(id));");
@@ -71,36 +70,39 @@ public class Database {
         lista.add("INSERT INTO Keskustelu (otsikko, kategoria) VALUES ('Induktio-oletus',5)");
         lista.add("INSERT INTO Keskustelu (otsikko, kategoria) VALUES ('Induktioväite',5)");
         lista.add("INSERT INTO Keskustelu (otsikko, kategoria) VALUES ('Väite on todistettu',5)");
-        lista.add("INSERT INTO Viesti (viesti,keskustelu) VALUES ('Väite on todistettu',1)");
-        lista.add("INSERT INTO Viesti (viesti,keskustelu) VALUES ('Geometriassa ja fysiikassa vektoreita',1)");
-        lista.add("INSERT INTO Viesti (viesti,keskustelu) VALUES ('Käytetään kuvaamaan suureita, joihin suuruuden',1)");
-        lista.add("INSERT INTO Viesti (viesti,keskustelu) VALUES ('Lisäksi liittyy määrätty suunta.',2)");
-        lista.add("INSERT INTO Viesti (viesti,keskustelu) VALUES ('Käytettyjen vektorien alkiot ovat reaalilukuja.',2)");
-        lista.add("INSERT INTO Viesti (viesti,keskustelu) VALUES ('Kolmiulotteisessa avaruusgeometriassa ',2)");
-        lista.add("INSERT INTO Viesti (viesti,keskustelu) VALUES ('Useimmissa fysikaalisissa sovelluksissa käytetyt vektorit kolmen reaaliluvun järjestettyinä',2)");
-        lista.add("INSERT INTO Viesti (viesti,keskustelu) VALUES ('Geometrisesti vektoreita voidaan kuvata janoilla, joiden toiseen päähän on tapana.',3)");
-        lista.add("INSERT INTO Viesti (viesti,keskustelu) VALUES ('Tällöin kuitenkin kaikki suunnatut janat, jotka ovat yhtä pitkiä',3)");
-        lista.add("INSERT INTO Viesti (viesti,keskustelu) VALUES ('Samansuuntaisia, katsotaan ekvivalenteiksi, toisin sanoen ne esittävät samaa vektoria.',3)");
-        lista.add("INSERT INTO Viesti (viesti,keskustelu) VALUES ('Lukujonona tällaisissa vektoreissa on vain kolme tai tasogeometriassa kaksi lukua',3)");
-        lista.add("INSERT INTO Viesti (viesti,keskustelu) VALUES ('Ensimmäinen vastaa tämän janan projektiota x-akselin suunnassa, toinen y-akseli',4)");
-        lista.add("INSERT INTO Viesti (viesti,keskustelu) VALUES ('Erikoistapauksena on nollavektori',5)");
-        lista.add("INSERT INTO Viesti (viesti,keskustelu) VALUES ('jossa kaikki nämä luvut ovat nollia. Sen suunta on määräämätön (mielivaltainen) ja pituus 0. ',6)");
-        lista.add("INSERT INTO Viesti (viesti,keskustelu) VALUES ('Geometriassa vektorilla voidaan kuvata annetun pisteen sijaintia suhteessa toiseen pisteeseen. ',7)");
-        lista.add("INSERT INTO Viesti (viesti,keskustelu) VALUES ('Pisteen paikkavektori kuvaa sen sijainnin suhteessa koordinaatiston origoon, ja sen alkioina ovat pisteen koordinaattien arvot. ',8)");
-        lista.add("INSERT INTO Viesti (viesti,keskustelu) VALUES ('Fysiikassa esimerkiksi nopeus on vektorisuure, ja sen itseisarvo, vauhti, on skalaari.',9)");
-        lista.add("INSERT INTO Viesti (viesti,keskustelu) VALUES ('Fysiikan eri aloilla tärkeitä vektorisuureita ovat myös kiihtyvyys ja voima sekä sähkö- ja magneettikenttien voimakkuudet. ',10)");
-        lista.add("INSERT INTO Viesti (viesti,keskustelu) VALUES ('Matriisi on matematiikassa suorakulmainen riveihin ja sarakkeisiin jaettu taulukko.',11)");
-        lista.add("INSERT INTO Viesti (viesti,keskustelu) VALUES ('Jonka alkiot ovat lukuja (usein reaali- tai kompleksilukuja) tai lausekkeita. ',12)");
-        lista.add("INSERT INTO Viesti (viesti,keskustelu) VALUES ('Matriiseja käytetään yleisesti kaksiulotteisen tiedon havainnollistamiseen sekä lineaaristen yhtälöryhmien käsittelyyn ja ratkaisemiseen.',13)");
-        lista.add("INSERT INTO Viesti (viesti,keskustelu) VALUES ('Jos rajoitutaan yhden muuttujan funktioihin',14)");
-        lista.add("INSERT INTO Viesti (viesti,keskustelu) VALUES ('Voidaan muutosnopeuden keskiarvoa kuvata funktion kuvaajan keskimääräiseksi jyrkkyydeksi. ',15)");
-        lista.add("INSERT INTO Viesti (viesti,keskustelu) VALUES ('Sitä havainnollistetaan esimerkiksi Suomen lukioiden matematiikan oppimäärissä kuvaajan sekantilla (keskimääräinen muutosnopeus)',16)");
-        lista.add("INSERT INTO Viesti (viesti,keskustelu) VALUES ('jonka kulmakerroin on kyseisellä välillä funktion jyrkkyyksien likiarvo.',16)");
-        lista.add("INSERT INTO Viesti (viesti,keskustelu) VALUES ('Mitä pienempi on sekantin rajaama väli.',16)");
-        lista.add("INSERT INTO Viesti (viesti,keskustelu) VALUES ('Sitä paremmin sen jyrkkyys vastaa funktion kuvaajan jyrkkyyksiä kyseisellä välillä.',17)");
-        lista.add("INSERT INTO Viesti (viesti,keskustelu) VALUES ('Lopulta, kun väliä pienennetään raja-arvon avulla pisteeksi.',18)");
-        lista.add("INSERT INTO Viesti (viesti,keskustelu) VALUES ('Saadaan derivaatta (muutosnopeus yhdessä kohdassa).',19)");
-        lista.add("INSERT INTO Viesti (viesti,keskustelu) VALUES ('Sitä havainnollistetaan yleensä tangentilla, jonka kulmakerroin on derivaatan arvon suuruinen.',20)");
+        lista.add("INSERT INTO Viesti (viesti,keskustelu) VALUES ('Mulla on ongelma, enkä tiedä miten se ratkaistaan t. Pertti', 1)");
+        lista.add("INSERT INTO Viesti (viesti,keskustelu) VALUES ('Vähän vaikee kuule auttaa, josset avaa ongelmaa vähän tarkemmin t. Martti', 1)");
+        lista.add("INSERT INTO Viesti (viesti,keskustelu) VALUES ('Samaistun ekaan postaajaan, integraalit on vaikeita. :( t. Ulla', 1)");
+        lista.add("INSERT INTO Viesti (viesti,keskustelu) VALUES ('Siis voisko oikeesti siihen hommata jonkun hiekotuksen t. Huolestunut', 2)");
+        lista.add("INSERT INTO Viesti (viesti,keskustelu) VALUES ('No samaa oon miettinyt t. Irkku', 2)");
+        lista.add("INSERT INTO Viesti (viesti,keskustelu) VALUES ('Kumpulan jäämäki on paras mäki! t. Mirkku', 2)");
+        lista.add("INSERT INTO Viesti (viesti,keskustelu) VALUES ('Liukastuin tänään... t. Pasi', 2)");
+        lista.add("INSERT INTO Viesti (viesti,keskustelu) VALUES ('Tykkään integroida!!! t. Fani', 3)");
+        lista.add("INSERT INTO Viesti (viesti,keskustelu) VALUES ('Jeejee! t. Fantsufani', 3)");
+        lista.add("INSERT INTO Viesti (viesti,keskustelu) VALUES ('Ihanaa, et täällä voi hehkuttaa integrointii! t. Sirppa', 3)");
+        lista.add("INSERT INTO Viesti (viesti,keskustelu) VALUES ('Superii hyperii t. Samppa', 3)");
+        lista.add("INSERT INTO Viesti (viesti,keskustelu) VALUES ('Voisko joku vähän avata tota derivaatan määritelmää. Tuntuu hankalalta. t. Heikkihei', 4)");
+        lista.add("INSERT INTO Viesti (viesti,keskustelu) VALUES ('Voiko derivoida liikaa t. Derivoija', 5)");
+        lista.add("INSERT INTO Viesti (viesti,keskustelu) VALUES ('En oikeen pääse alkun. Miten aloittaa derivointi? t. Essi', 6)");
+        lista.add("INSERT INTO Viesti (viesti,keskustelu) VALUES ('Miten matriiseja voi hyödyntää dynaamisessa ohjelmoinnissa? t. Reiska', 7)");
+        lista.add("INSERT INTO Viesti (viesti,keskustelu) VALUES ('Onks matriisilla jotain yhteyttä matrixiin? t. Tietämätön', 8)");
+        lista.add("INSERT INTO Viesti (viesti,keskustelu) VALUES ('Haxxxxxhahhhxhxhxhhxx t. Hax', 9)");
+        lista.add("INSERT INTO Viesti (viesti,keskustelu) VALUES ('Mikähän pointti tässä oli? t. Lopeta', 9)");
+        lista.add("INSERT INTO Viesti (viesti,keskustelu) VALUES ('Matriisit kunniaan!!! t. Tumppi', 10)");
+        lista.add("INSERT INTO Viesti (viesti,keskustelu) VALUES ('Jos Aladdinin matto kulkee vektoreiden x ja y suuntaan, pääseekö Aladdin lopulta takaisin basaariin? t. Irene', 11)");
+        lista.add("INSERT INTO Viesti (viesti,keskustelu) VALUES ('Oliks julius sitseil t. Hulius', 12)");
+        lista.add("INSERT INTO Viesti (viesti,keskustelu) VALUES ('Jos on vektroitraktori nii mitä tapahtuu? t. Jussi', 13)");
+        lista.add("INSERT INTO Viesti (viesti,keskustelu) VALUES ('Voiko kaiken oikeestaan ajatella vektoreina? t. Jonna', 14)");
+        lista.add("INSERT INTO Viesti (viesti,keskustelu) VALUES ('Onko jotain, mikä ei ole vektori? Tällaista mietin. t. Jonna', 15)");
+        lista.add("INSERT INTO Viesti (viesti,keskustelu) VALUES ('Päättely toimii muuten hyvin. Onko siis ok? t. Olli', 16)");
+        lista.add("INSERT INTO Viesti (viesti,keskustelu) VALUES ('Tämä on alkuaskel. t. Todistaja', 17)");
+        lista.add("INSERT INTO Viesti (viesti,keskustelu) VALUES ('Seuraavaksi kai tulee induktio-oletus? t. Apuri', 17)");
+        lista.add("INSERT INTO Viesti (viesti,keskustelu) VALUES ('Ja sitten todistetaan väite todeksi induktio-oletuksen avulla. Helppo homma! t. Jeesari', 17)");
+        lista.add("INSERT INTO Viesti (viesti,keskustelu) VALUES ('Tämä on induktio-oletus. t. Todistaja', 18)");
+        lista.add("INSERT INTO Viesti (viesti,keskustelu) VALUES ('Sitten väitellään induktiosta t. Todistaja', 19)");
+        lista.add("INSERT INTO Viesti (viesti,keskustelu) VALUES ('Jee! t. Apuri', 19)");
+        lista.add("INSERT INTO Viesti (viesti,keskustelu) VALUES ('Kaikki on todistettu! Siisti juttu! t. Todistaja', 20)");
+
         return lista;
     }
 }
