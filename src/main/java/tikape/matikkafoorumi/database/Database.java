36b6ee7ed015313be3ce2bfd18aeb6f0e13e5854
Maaret <maaret.ihamaki@hotmail.com>
2017-03-03 19:20:28 +0200

    kommentteja pois

--- src/main/java/tikape/matikkafoorumi/database/Database.java
+++ src/main/java/tikape/matikkafoorumi/database/Database.java
@@ -4,10 +4,6 @@ import java.sql.*;
 import java.util.ArrayList;
 import java.util.List;
 
-/*
-Täällä luodaan yhteys meidän foorumin tietokantaan ja jos tietokantaa ei vielä 
-ole luotu, niin tämä koodi myös luo sen. 
- */
 public class Database {
 
     private String databaseAddress;
@@ -23,18 +19,15 @@ public class Database {
     public void init() {
         List<String> lauseet = sqliteLauseet();
 
-        // "try with resources" sulkee resurssin automaattisesti lopuksi
         try (Connection conn = getConnection()) {
             Statement st = conn.createStatement();
 
-            // suoritetaan komennot
             for (String lause : lauseet) {
                 System.out.println("Running command >> " + lause);
                 st.executeUpdate(lause);
             }
 
         } catch (Throwable t) {
-            // jos tietokantataulu on jo olemassa, ei komentoja suoriteta
             System.out.println("Error >> " + t.getMessage());
         }
     }
